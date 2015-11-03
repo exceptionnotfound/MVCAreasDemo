4651d7dd710dd1ee00d84a296a573f9bcdd46ef3
exceptionnotfound <mpjones428@gmail.com>
2015-11-03 08:20:34 -0700

    Initial creation of project

---
+++ MVCAreasDemo/Areas/Billing/BillingAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MVCAreasDemo.Areas.Billing
+{
+    public class BillingAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Billing";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Billing_default",
+                "Billing/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
